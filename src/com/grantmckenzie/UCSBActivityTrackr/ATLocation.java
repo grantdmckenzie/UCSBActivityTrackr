8bc3558841ee707790f708e44eeabf46504d5216
Grant McKenzie <grantdmckenzie@gmail.com>
2011-05-16 23:00:42 -0700

    updated ucsbtracker

--- src/com/grantmckenzie/UCSBActivityTrackr/ATLocation.java
+++ src/com/grantmckenzie/UCSBActivityTrackr/ATLocation.java
@@ -1,7 +1,23 @@
 package com.grantmckenzie.UCSBActivityTrackr;
 
+import java.io.IOException;
 import java.sql.Timestamp;
+import static android.provider.BaseColumns._ID;
+
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.List;
+import java.util.UUID;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
@@ -11,26 +27,26 @@ import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.location.LocationProvider;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.os.IBinder;
 import android.preference.PreferenceManager;
 import android.telephony.TelephonyManager;
 import android.widget.Toast;
-import android.os.CountDownTimer;
 
-public class ATLocation extends Service {
+public class ATLocation extends Service implements LocationListener{
+	
 	private LocationManager locationManager;
-	
+	private String best;
 	private Location currentLocation;
-	private LocationListener listenerCoarse;
-	private LocationListener listenerFine;
-	
-	Criteria fine = null;
-	Criteria coarse = null;
-	
-	private boolean isFine;
+	private String handler = "http://geogremlin.geog.ucsb.edu/grantm/android_server.php";
+
+	Criteria crit = null;
+
 	
 	TelephonyManager tm;
+	private ConnectivityManager connectivity;
     String deviceId;
     private String vals;
     
@@ -43,37 +59,20 @@ public class ATLocation extends Service {
 	public void onCreate() {
 
 		locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+		crit = new Criteria();
+		best = locationManager.getBestProvider(crit, true);
+		currentLocation = locationManager.getLastKnownLocation(best);
 		
-		fine = new Criteria();
-		fine.setAccuracy(Criteria.ACCURACY_FINE);
-		coarse = new Criteria();
-		coarse.setAccuracy(Criteria.ACCURACY_COARSE);
-		isFine = true;
-		if(locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
-			currentLocation = locationManager.getLastKnownLocation(locationManager.getBestProvider(fine, true));
-		} else {
-			currentLocation = locationManager.getLastKnownLocation(locationManager.getBestProvider(coarse, true));
-			Toast.makeText(getBaseContext(),"GPS Disabled",Toast.LENGTH_LONG).show();
-		}
-
-		listenerFine = new MyLocationListener();
-		listenerCoarse = new MyLocationListener();
-		
-		new CountDownTimer(20000, 1000) {
-
-		     public void onTick(long millisUntilFinished) {
-		    	 // Toast.makeText(getBaseContext(),"seconds remaining: " + millisUntilFinished / 1000,Toast.LENGTH_SHORT).show();
-		     }
-
-		     public void onFinish() {
-		    	 locationManager.removeUpdates(listenerFine);
-		    	 Toast.makeText(getBaseContext(),"Switching to Coarse Provider" ,Toast.LENGTH_SHORT).show();
-		    	 locationManager.requestLocationUpdates(locationManager.getBestProvider(coarse, true),1000, 300, listenerCoarse);
-		     }
-		  }.start();
-		
-		
-
+		tm = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
+		connectivity = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+		// Get unique device ID
+		String tmDevice, tmSerial, androidId;
+	    tmDevice = "" + tm.getDeviceId();
+	    tmSerial = "" + tm.getSimSerialNumber();
+	    androidId = "" + android.provider.Settings.Secure.getString(getContentResolver(), android.provider.Settings.Secure.ANDROID_ID);
+	    UUID deviceUuid = new UUID(androidId.hashCode(), ((long)tmDevice.hashCode() << 32) | tmSerial.hashCode());
+	    deviceId = deviceUuid.toString();
+	    
 		/* best = locationManager.getBestProvider(criteria, true);
 		Location location = locationManager.getLastKnownLocation(best);
 		String Text = "Best Provider is: "+best+"\nLast known coords:\nLat= " + location.getLatitude() + "\nLong= " + location.getLongitude();
@@ -83,54 +82,41 @@ public class ATLocation extends Service {
 	@Override
 	public void onDestroy() {
 		Toast.makeText(this, "Location Service Stopped", Toast.LENGTH_SHORT).show();
-		locationManager.removeUpdates(listenerCoarse);
-		locationManager.removeUpdates(listenerFine);
+		locationManager.removeUpdates(this);
 	}
 	
 	@Override
 	public void onStart(Intent intent, int startid) {
 		Toast.makeText(this, "Location Service Started", Toast.LENGTH_SHORT).show();
-		 // locationManager.requestLocationUpdates(locationManager.getBestProvider(coarse, true),500, 300, listenerCoarse);
-		if (isFine)
-			locationManager.requestLocationUpdates(locationManager.getBestProvider(fine, true), 0, 0, listenerFine);
-
+		locationManager.requestLocationUpdates(best, 20000, 0, this);
 	}
 	
-	public class MyLocationListener implements LocationListener {
-		
-		@Override
-		public void onLocationChanged(Location loc) {
+	public void onLocationChanged(Location loc) {
 			
 			Timestamp ts = new Timestamp(Calendar.getInstance().getTime().getTime());	
 			double lat = loc.getLatitude();
 			double lon = loc.getLongitude();
-			if(isFine)
-				vals = lat + "," + lon + "," + ts.toString() + ",1|";
-			else 
-				vals = lat + "," + lon + "," + ts.toString() + ",0|";
-			storeData(vals);
-		}
-
-		@Override
-		public void onProviderDisabled(String provider) {
-			// Toast.makeText( getApplicationContext(),"Gps Disabled",Toast.LENGTH_SHORT ).show();
-			
-		}
-
-		@Override
-		public void onProviderEnabled(String provider) {
-			// Toast.makeText( getApplicationContext(),"Gps Enabled",Toast.LENGTH_SHORT).show();
-		}
-
-		@Override
-		public void onStatusChanged(String provider, int status, Bundle extras) {
-			Toast.makeText( getApplicationContext(),"Location Service using: "+provider,Toast.LENGTH_SHORT).show();
-		}
-
+			// Toast.makeText(this, ""+lat+", "+lon, Toast.LENGTH_SHORT).show();
+			// vals = lat + "," + lon + "," + ts.toString();
+			storeData(lat+"", lon+"", ts.toString());
 	}
+
+	public void onProviderDisabled(String provider) {
+			// Toast.makeText( getApplicationContext(),"Gps Disabled",Toast.LENGTH_SHORT ).show();
+	}
+
+	public void onProviderEnabled(String provider) {
+			// Toast.makeText( getApplicationContext(),"Gps Enabled",Toast.LENGTH_SHORT).show();
+	}
+
+	public void onStatusChanged(String provider, int status, Bundle extras) {
+			// Toast.makeText( getApplicationContext(),"Location Service using: "+provider,Toast.LENGTH_SHORT).show();
+	}
+
 	
-	private void storeData(String data) {
-		SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(this);
+	private void storeData(String lat, String lon, String timest) {
+		
+		/* SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(this);
 		
 		String st_locdata = settings.getString("ST_LOCDATA", "");
 		int st_coordcounter = settings.getInt("ST_COORDS", 0);
@@ -143,9 +129,65 @@ public class ATLocation extends Service {
 	    editor.putInt("ST_COORDS", st_coordcounter);
 	    
 	    editor.commit();
-	    Toast.makeText( getApplicationContext(),"Coords: "+st_coordcounter,Toast.LENGTH_SHORT).show();
-	    locationManager.removeUpdates(listenerFine);
-	    locationManager.removeUpdates(listenerCoarse);
+	    Toast.makeText( getApplicationContext(),"Coords: "+data,Toast.LENGTH_SHORT).show(); */
+	    
+		 if (isNetworkAvailable(getApplicationContext())) {
+			 if (lat != "") {
+				 String response = sendLocation(deviceId, lat, lon, timest);
+				 if (response != "0") {
+					 /* editor = settings.edit();
+					 editor.putString("ST_LOCDATA", "");
+					 editor.putInt("ST_COORDS", 0);
+					 editor.putString("ST_PHONE", "");
+					 editor.commit(); */
+					 Toast.makeText( getApplicationContext(),"Data sent to server.",Toast.LENGTH_SHORT).show();
+				 }
+			 } else {
+				 Toast.makeText( getApplicationContext(),"No new data to send.",Toast.LENGTH_SHORT).show();
+			 }
+		 } else {
+			 Toast.makeText( getApplicationContext(),"No Data Connection.\nData not sent to server.",Toast.LENGTH_SHORT).show();
+		 }
+
+	}
+	
+	public boolean isNetworkAvailable(Context context) {
+	    if (connectivity != null) {
+	       NetworkInfo[] info = connectivity.getAllNetworkInfo();
+	       if (info != null) {
+	          for (int i = 0; i < info.length; i++) {
+	             if (info[i].getState() == NetworkInfo.State.CONNECTED) {
+	                return true;
+	             }
+	          }
+	       }
+	    } 
+	    return false;
+	 }
+	
+	private String sendLocation(String uid, String lat, String lon, String timest) {
+		HttpClient httpclient = new DefaultHttpClient();  
+	    HttpPost httppost = new HttpPost(handler);  
+	    HttpResponse response = null;
+	    
+	    try {  
+	        // Add your data  
+	        List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>();  
+	        nameValuePairs.add(new BasicNameValuePair("uid", uid));  
+	        nameValuePairs.add(new BasicNameValuePair("lat", lat));
+	        nameValuePairs.add(new BasicNameValuePair("lon", lon));
+	        nameValuePairs.add(new BasicNameValuePair("t", timest));
+	        httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));  
+	  
+	        // Execute HTTP Post Request  
+	        response = httpclient.execute(httppost);  
+	          
+	    } catch (ClientProtocolException e) {  
+	        // TODO Auto-generated catch block  
+	    } catch (IOException e) {  
+	        // TODO Auto-generated catch block  
+	    }  
+		return response.toString();
 	}
 }
 
--- src/com/grantmckenzie/UCSBActivityTrackr/UCSBActivityTrackr.java
+++ src/com/grantmckenzie/UCSBActivityTrackr/UCSBActivityTrackr.java
@@ -18,8 +18,6 @@ import android.widget.ToggleButton;
 public class UCSBActivityTrackr extends Activity implements OnClickListener {
 
   ToggleButton buttonLocation, buttonSendData;
-  private PendingIntent sdpendingIntent, locpendingIntent;
-
   private TelephonyManager tm;
   private String deviceId;
   private int resultid = 0;
@@ -42,50 +40,29 @@ public class UCSBActivityTrackr extends Activity implements OnClickListener {
     buttonLocation.setOnClickListener(this);
     buttonLocation.setText("Location Off");
  
-    buttonSendData = (ToggleButton) findViewById(R.id.toggleSendData);
-    buttonSendData.setOnClickListener(this);
-    buttonSendData.setText("Send Data Off");
     
   }
  
-  
-  
   public void onClick(View src) {
     switch (src.getId()) {
     case R.id.toggleLocation:
     	if (!buttonLocation.isChecked()) {
-    		AlarmManager alarmManager = (AlarmManager)getSystemService(ALARM_SERVICE);
-    	    alarmManager.cancel(locpendingIntent);
-    	    Toast.makeText(this, "Location Off", Toast.LENGTH_LONG).show(); 
+    		/* AlarmManager alarmManager = (AlarmManager)getSystemService(ALARM_SERVICE);
+    	    alarmManager.cancel(locpendingIntent); */
+    		stopService(new Intent(this, ATLocation.class));
+    	    // Toast.makeText(this, "Location Off", Toast.LENGTH_LONG).show(); 
     	    buttonLocation.setText("Location Off");
     	} else {
     		buttonLocation.setText("Location On");
     		
-    		long firstTime = SystemClock.elapsedRealtime();
+    		/* long firstTime = SystemClock.elapsedRealtime();
 			Intent myIntent = new Intent(this, ATLocation.class);
-			locpendingIntent = PendingIntent.getService(this, 0, myIntent, 0);
-			AlarmManager alarmManager = (AlarmManager)getSystemService(ALARM_SERVICE);
-			alarmManager.setRepeating(AlarmManager.ELAPSED_REALTIME_WAKEUP, firstTime, 300000, locpendingIntent);
+			locpendingIntent = PendingIntent.getService(this, 0, myIntent, 0); */
+    		startService(new Intent(this, ATLocation.class));
+			/* AlarmManager alarmManager = (AlarmManager)getSystemService(ALARM_SERVICE);
+			alarmManager.setRepeating(AlarmManager.ELAPSED_REALTIME_WAKEUP, firstTime, 300000, locpendingIntent); */
     	}
         break;
-    case R.id.toggleSendData:
-    	if (!buttonSendData.isChecked()) {
-    	    // stopService(new Intent(this, STService.class));
-    	    buttonSendData.setText("Send Data Off");
-    	    AlarmManager alarmManager = (AlarmManager)getSystemService(ALARM_SERVICE);
-    	    alarmManager.cancel(sdpendingIntent);
-    	    Toast.makeText(this, "Send Data Service Stopped", Toast.LENGTH_LONG).show(); 
-    	    
-    	} else {
-    		// startService(new Intent(this, S.class));
-    		buttonSendData.setText("Send Data On");
-    		long firstTime = SystemClock.elapsedRealtime();
-			Intent myIntent = new Intent(this, ATUpload.class);
-			sdpendingIntent = PendingIntent.getService(this, 0, myIntent, 0);
-			AlarmManager alarmManager = (AlarmManager)getSystemService(ALARM_SERVICE);
-			alarmManager.setRepeating(AlarmManager.ELAPSED_REALTIME_WAKEUP, firstTime, 3600*1000, sdpendingIntent);
-    	}
-    	break;
     }
   }
 }
