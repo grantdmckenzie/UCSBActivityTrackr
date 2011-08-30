b556ef6657382a8ec7c8d394a5e5c1bdd89c3c24
Grant McKenzie <grantdmckenzie@gmail.com>
2011-08-30 00:24:19 -0700

    New layout for location questionnaire.

--- gen/com/grantmckenzie/UCSBActivityTrackr/R.java
+++ gen/com/grantmckenzie/UCSBActivityTrackr/R.java
@@ -14,18 +14,30 @@ public final class R {
         public static final int icon=0x7f020000;
     }
     public static final class id {
-        public static final int Login=0x7f050005;
-        public static final int Password=0x7f050004;
-        public static final int TextView01=0x7f050000;
-        public static final int Username=0x7f050002;
-        public static final int locResults=0x7f050006;
-        public static final int textUsername=0x7f050001;
-        public static final int txtPassword=0x7f050003;
+        public static final int Login=0x7f050006;
+        public static final int Password=0x7f050005;
+        public static final int TextView01=0x7f050001;
+        public static final int Username=0x7f050003;
+        public static final int editText1=0x7f050010;
+        public static final int header=0x7f050008;
+        public static final int home=0x7f05000e;
+        public static final int item1=0x7f050000;
+        public static final int layout=0x7f050009;
+        public static final int linearLayout1=0x7f05000b;
+        public static final int listview=0x7f05000a;
+        public static final int locResults=0x7f05000f;
+        public static final int main=0x7f050007;
+        public static final int notactivity=0x7f05000c;
+        public static final int notlisted=0x7f05000d;
+        public static final int textUsername=0x7f050002;
+        public static final int txtPassword=0x7f050004;
     }
     public static final class layout {
-        public static final int main=0x7f030000;
-        public static final int questionnaire=0x7f030001;
-        public static final int questionnaire_old=0x7f030002;
+        public static final int grid_item=0x7f030000;
+        public static final int main=0x7f030001;
+        public static final int questionnaire=0x7f030002;
+        public static final int questionnaire_old=0x7f030003;
+        public static final int questionnaire_other=0x7f030004;
     }
     public static final class string {
         public static final int app_name=0x7f040001;
--- src/com/grantmckenzie/UCSBActivityTrackr/ATQuestionnaire.java
+++ src/com/grantmckenzie/UCSBActivityTrackr/ATQuestionnaire.java
@@ -7,43 +7,100 @@
  */
 
 package com.grantmckenzie.UCSBActivityTrackr;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
-import android.app.ListActivity;
+import android.app.Activity;
+import android.app.AlertDialog;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
-import android.widget.ArrayAdapter;
-import android.widget.EditText;
+import android.widget.Button;
 import android.widget.ListView;
+import android.widget.SimpleAdapter;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class ATQuestionnaire extends ListActivity {
+public class ATQuestionnaire extends Activity implements OnClickListener {
+    /** Called when the activity is first created. */
+	private ListView lv;
+	Button buttonNotActivity;
+	Button buttonHome;
+	Button buttonNotListed;
 	
-	  EditText locResults;
-	
-	  @Override
-	  public void onCreate(Bundle savedInstanceState) {
-		  super.onCreate(savedInstanceState);
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.questionnaire);
+        lv= (ListView)findViewById(R.id.listview);
 
-		  Bundle b = this.getIntent().getExtras();
-		  String response = b.getString("locations");
-		  String lines[] = response.split("\\r?\\n");
-		  lines[0] = " Where are you?\n Please select from the nearby places below:";
-		  
-		  setListAdapter(new ArrayAdapter<String>(this, R.layout.questionnaire, lines));
+        buttonNotActivity = (Button) findViewById(R.id.notactivity);
+        buttonNotActivity.setOnClickListener(this);
+        buttonNotListed = (Button) findViewById(R.id.notlisted);
+        buttonNotListed.setOnClickListener(this);
+        buttonHome = (Button) findViewById(R.id.home);
+        buttonHome.setOnClickListener(this);
+        
+        // create the grid item mapping
+        String[] from = new String[] {"col_1"};
+        int[] to = new int[] { R.id.item1 };
 
-		  ListView lv = getListView();
-		  lv.setTextFilterEnabled(true);
+        Bundle b = this.getIntent().getExtras();
+		String response = b.getString("locations");
+        // String response = "hello world 1\nhello world 2\nhello world 1\nhello world 2\nhello world 1\nhello world 2\nhello world 1\nhello world 2\nhello world 1\nhello world 2"; 
+		String lines[] = response.split("\\r?\\n");
+		
+        // prepare the list of all records
+        List<HashMap<String, String>> fillMaps = new ArrayList<HashMap<String, String>>();
+        for(int i = 1; i < lines.length; i++){
+        	HashMap<String, String> map = new HashMap<String, String>();
+        	map.put("col_1", lines[i]);
+        	fillMaps.add(map);
+        }
 
-		  lv.setOnItemClickListener(new OnItemClickListener() {
-		    public void onItemClick(AdapterView<?> parent, View view,
-		        int position, long id) {
-		      // When clicked, show a toast with the TextView text
-		      Toast.makeText(getApplicationContext(), ((TextView) view).getText(),
-		          Toast.LENGTH_SHORT).show();
-		    }
-		  });
-	  }
+        // fill in the grid_item layout
+        SimpleAdapter adapter = new SimpleAdapter(this, fillMaps, R.layout.grid_item, from, to);
+        lv.setAdapter(adapter);
+        lv.setOnItemClickListener(new OnItemClickListener() {
+        	
+	        @Override
+	        public void onItemClick(AdapterView<?> parent, View v, int position, long id)
+	        { 
+	        	String stuff = lv.getItemAtPosition(position).toString();
+	        	String[] name = stuff.split("=");
+	            AlertDialog.Builder adb=new AlertDialog.Builder(ATQuestionnaire.this);
+	        	adb.setTitle("TravelerServ");
+	        	adb.setMessage("Selected Location: "+name[1].replace('}', ' '));
+	        	adb.setPositiveButton("Ok", null);
+	        	adb.show(); 
+	        }
+        });
+    }
+
+	@Override
+	public void onClick(View src) {
+		if (src.getId() == R.id.notactivity) {
+			finish();
+		} else if (src.getId() == R.id.notlisted) {
+			LayoutInflater factory = LayoutInflater.from(this);
+            final View textEntryView = factory.inflate(R.layout.questionnaire_other, null);
+            AlertDialog.Builder adb=new AlertDialog.Builder(ATQuestionnaire.this);
+                adb.setTitle("TravelerServ");
+                adb.setView(textEntryView);
+                adb.setPositiveButton("Yes", null);
+                adb.show();
+		
+		} else if (src.getId() == R.id.home) {
+            AlertDialog.Builder adb=new AlertDialog.Builder(ATQuestionnaire.this);
+        	adb.setTitle("TravelerServ");
+        	adb.setMessage("Would you like to set your current location as HOME?");
+        	adb.setPositiveButton("Yes", null);
+        	adb.setNegativeButton("No", null);
+        	adb.show(); 
+		}
+	}
 }
